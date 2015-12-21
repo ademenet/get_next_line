859083354a5db7ac8b9f7347d5b42d4bdfcca713
Alain DEMENET <ademenet@e2r8p8.42.fr>
2015-12-21 11:10:22 +0100

    Ajout proto et define

--- get_next_line.h
+++ get_next_line.h
@@ -6,11 +6,13 @@
 /*   By: ademenet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/21 09:54:44 by ademenet          #+#    #+#             */
-/*   Updated: 2015/12/21 09:56:01 by ademenet         ###   ########.fr       */
+/*   Updated: 2015/12/21 10:57:30 by ademenet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+# define BUFF_SIZE 32
 
+int		get_next_line(int const fd, char **line)
 #endif
