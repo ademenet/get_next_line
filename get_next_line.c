d65692e7d326851484760ee0e3c87aa7815f3826
Alain DEMENET <ademenet@e2r5p13.42.fr>
2016-02-02 18:58:53 +0100

    En plein debug : tmp[fd] est reinitialise a chaque rappel de la fonction donc nous perdons les chars ce qui cree un decalage.

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: ademenet <ademenet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/21 09:53:51 by ademenet          #+#    #+#             */
-/*   Updated: 2016/02/02 15:25:15 by ademenet         ###   ########.fr       */
+/*   Updated: 2016/02/02 18:52:37 by ademenet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,28 +24,46 @@ static unsigned int			line_length(char *tmp)
 	return (i);
 }
 
+// Probleme : lorsque la fonction est rappelee tmp[fd] se reinitialise
+// le static ne fonctionne pas
 int							get_next_line(int const fd, char **line)
 {
-	static char		**tmp = NULL;
+	static char		**tmp;
 	char			buf[BUFF_SIZE + 1];
 	int				read_val;
 
 	if (fd < 0 || BUFF_SIZE <= 0 || !line)
 		return (-1);
-	if ((tmp = (char **)malloc(sizeof(char *) * 257)) == NULL)
-		return (-1);
-	if (!(tmp[fd]) && ((tmp[fd] = ft_strnew(BUFF_SIZE)) == NULL))
-		return (-1);
+	if (!tmp)
+	{
+		ft_putstr("je cree tmp"); ft_putstr("\n"); getchar();
+		if ((tmp = (char **)malloc(sizeof(char *) * 257)) == NULL)
+			return (-1);
+	}
+	if (!(tmp[fd]))
+	{
+		ft_putstr("je cree tmp[fd]"); ft_putstr("\n"); getchar();
+		if ((tmp[fd] = ft_strnew(0)) == NULL)
+			return (-1);
+	}
+	ft_putstr("tmp[fd] avant la boucle:"); ft_putstr(tmp[fd]); ft_putstr("\n"); getchar();
+
 	while (!(ft_strchr(buf, '\n')) && (read_val = read(fd, buf, BUFF_SIZE)) > 0)
 	{
+		buf[read_val] = '\0';
+		ft_putstr("tmp[fd] avant le join:"); ft_putstr(tmp[fd]); ft_putstr("\n"); getchar();
 		tmp[fd] = ft_strjoin(tmp[fd], buf);
+		ft_putstr("tmp[fd] apres le join:"); ft_putstr(tmp[fd]); ft_putstr("\n"); getchar();
 	}
 	*line = ft_strsub(tmp[fd], 0, line_length(tmp[fd]));
 	if (ft_strchr(buf, '\n'))
 	{
-		tmp[fd] = ft_strchr(buf, '\n');
+		ft_putstr("tmp[fd] avant le strchr:"); ft_putstr(tmp[fd]); ft_putstr("\n"); getchar();
+		tmp[fd] = ft_strchr(buf, '\n') + 1;
+		ft_putstr("tmp[fd] apres le strchr:"); ft_putstr(tmp[fd]); ft_putstr("\n"); getchar();
 		return (1);
 	}
 	tmp[fd] += line_length(tmp[fd]);
+	ft_putstr("tmp[fd] juste avant la fin:"); ft_putstr(tmp[fd]); ft_putstr("\n"); getchar();
 	return (read_val ? 1 : 0);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,13 +6,13 @@
 /*   By: ademenet <ademenet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/21 09:54:44 by ademenet          #+#    #+#             */
-/*   Updated: 2016/02/02 15:45:18 by ademenet         ###   ########.fr       */
+/*   Updated: 2016/02/02 18:06:49 by ademenet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define BUFF_SIZE 3
+# define BUFF_SIZE 4
 
 # include <sys/types.h>
 # include <sys/uio.h>
